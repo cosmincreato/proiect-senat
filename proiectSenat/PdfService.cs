90c86a30b6b28e01aec1b97fc456e985cf4041a4
diap <diap@LAPTOP-GN1TE46R>
2025-09-09 11:18:13 +0300

    Add project files.

---
+++ proiectSenat/PdfService.cs
@@ -0,0 +1,50 @@
+﻿using UglyToad.PdfPig;
+using UglyToad.PdfPig.Content;
+using System.Text;
+
+
+namespace proiectSenat
+{
+    internal static class PdfService
+    {
+
+        private const string PDF_DIR_PATH = @"C:\Users\diap\source\repos\proiectSenat\proiectSenat\input\";
+        private const string OUTPUT_DIR_PATH = @"C:\Users\diap\source\repos\proiectSenat\proiectSenat\output\";
+
+        public static void ConvertToText()
+        {
+            if (!Directory.Exists(OUTPUT_DIR_PATH))
+            {
+                Directory.CreateDirectory(OUTPUT_DIR_PATH);
+            }
+            var pdfs = Directory.EnumerateFiles(PDF_DIR_PATH, "*.pdf");
+            Console.WriteLine($"{pdfs.Count<string>().ToString()} PDF-uri gasite in folderul input.");
+            foreach (var pdf in pdfs)
+            {
+                var sb = new StringBuilder();
+                // Extragem textul din PDF folosind PdfPig
+                // am putea folosi iText, dar nu este free for commercial use
+                using (PdfDocument document = PdfDocument.Open(pdf))
+                {
+                    foreach (Page page in document.GetPages())
+                    {
+                        IEnumerable<Word> words = page.GetWords();
+                        foreach (var word in words)
+                        {
+                            sb.Append(word.Text);
+                            sb.Append(' ');
+                        }
+                        sb.AppendLine("");
+                    }
+                }
+                // Adaugam fisierul .txt in folderul de output
+                string fileName = Path.GetFileNameWithoutExtension(pdf) + ".txt";
+                string outputPath = Path.Combine(OUTPUT_DIR_PATH, fileName);
+                File.WriteAllText(outputPath, sb.ToString());
+                Console.WriteLine($"{outputPath}");
+            }
+            // RAG Architecture cu n8n si MCP
+            // Antrenam modelul pe GPU
+        }
+    }
+}
---
+++ proiectSenat/Program.cs
@@ -0,0 +1,46 @@
+﻿using proiectSenat;
+using System.Xml;
+using System.Text;
+
+class Program
+{
+    private static HttpClient sharedClient = new()
+    {
+        BaseAddress = new Uri("https://www.senat.ro/"),
+    };
+
+    private static async Task GetProjects(string an = "", string nrSenat = "", string nrDeputati = "")
+    {
+        try
+        {
+            string endpoint = $"exportdata.asmx/ListaProiectelor?An={an}&NrSenat={nrSenat}&NrDeputati={nrDeputati}";
+            HttpResponseMessage response = await sharedClient.GetAsync(endpoint);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine("Response received:");
+                Console.WriteLine(PrintXML(content));
+            }
+            else
+            {
+                Console.WriteLine($"Request failed with status: {response.StatusCode}");
+            }
+        }
+        catch (HttpRequestException e)
+        {
+            Console.WriteLine($"HTTP request error: {e.Message}");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"General error: {e.Message}");
+        }
+    }
+
+    
+    static async Task Main()
+    {
+        PdfService.ConvertToText();
+        await GetProjects("2025");
+    }
+}
